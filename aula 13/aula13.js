bcbc3c496f949fb952590ced7c24085c760e8814
Gustavo Almeida <gustavo_80b@hotmail.com>
2022-05-31 00:33:43 -0300

    Novas aulas

---
+++ aula 13/aula13.js
@@ -0,0 +1,8 @@
+
+// Indíce        012345678910111213141516  
+let umastring = "Um texto qualquer"
+
+console.log(umastring[16])
+
+// Para saber em qual indice inicia determinada palavra
+console.log(umastring.indexOf('texto'))

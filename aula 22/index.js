b500287a8530e0154d8748630e1b482ed26a1ecf
Gustavo Almeida <gustavo_80b@hotmail.com>
2022-06-02 22:23:23 -0300

    Aula 22

---
+++ aula 22/index.js
@@ -0,0 +1,13 @@
+/*
+Operadores Lógicos
+
+&& -> AND -> E -> todas as expressões precisam ser verdadeiras para retornar TRUE
+|| -> OR -> OU -> 
+! -> NOT -> NÃO
+
+*/
+
+const expressaoAnd = true && true
+const expressaoOr = true || false
+
+console.log()

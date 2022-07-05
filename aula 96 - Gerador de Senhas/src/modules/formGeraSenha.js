1703067cc48815404d0b48bbbfb6b9328852d5af
Guss80 <gustavo_80b@hotmail.com>
2022-07-04 22:33:05 -0300

    Aulas 95 e 96

---
+++ aula 96 - Gerador de Senhas/src/modules/formGeraSenha.js
@@ -0,0 +1,3 @@
+import GeraSenha from "./geradores.js";
+
+const senhaGerada = document.querySelector('.senha-gerada');
---
+++ aula 96 - Gerador de Senhas/src/modules/geradores.js
@@ -0,0 +1,21 @@
+const rand = (min, max) => Math.floor(Math.random() * (max-min) + min);
+const geraMaiuscula = () => String.fromCharCode(rand(65, 91));
+const geraMinuscula = () => String.fromCharCode(rand(97, 123));
+const geraNumeros = () => String.fromCharCode(rand(48, 58));
+const simbolos = ',.;:~^[]{}!@#$%¨&*()-=+';
+const geraSimbolos = () => simbolos[rand(0, simbolos.length)];
+
+export default function geraSenha(qtd, maiuscula, minuscula, numero, simbolo){
+    const senhaArray = [];
+    qtd = Number(qtd);
+
+    for(let i = 0; i < qtd; i++ ){
+        maiuscula && senhaArray.push(geraMaiuscula());
+        minuscula && senhaArray.push(geraMinuscula());
+        numero && senhaArray.push(geraNumeros());        
+        simbolo && senhaArray.push(geraSimbolos());
+    }
+
+    return senhaArray.join('').slice(0, qtd);
+}
+

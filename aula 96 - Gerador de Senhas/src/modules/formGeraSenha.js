4631c42aa83e6c923a2f76c221fd0a251bbb185c
Guss80 <gustavo_80b@hotmail.com>
2022-07-07 19:33:23 -0300

    Novos arquivos

--- aula 96 - Gerador de Senhas/src/modules/formGeraSenha.js
+++ aula 96 - Gerador de Senhas/src/modules/formGeraSenha.js
@@ -1,3 +1,26 @@
-import GeraSenha from "./geradores.js";
+import geraSenha from "./geradores.js";
 
 const senhaGerada = document.querySelector('.senha-gerada');
+const qtdCaracteres = document.querySelector('.qtd-caracteres');
+const chkMaiusculas = document.querySelector('.chk-maiusculas');
+const chkMinusculas = document.querySelector('.chk-minusculas');
+const chkNumeros = document.querySelector('.chk-numeros');
+const chkSimbolos = document.querySelector('.chk-simbolos');
+const btGerarSenha = document.querySelector('.gerar-senha');
+
+export default () => {
+    btGerarSenha.addEventListener('click', () => {
+        senhaGerada.innerHTML = gera();
+    })
+}
+
+function gera(){
+    const senha = geraSenha(
+        qtdCaracteres.value,
+        chkMaiusculas.checked,
+        chkMinusculas.checked,
+        chkNumeros.checked,
+        chkSimbolos.checked
+    );
+    return senha || 'Nada selecionado';
+}
